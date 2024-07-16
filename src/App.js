c8f6824a019b41a2b90bb4151e7e12234e9b939b
borgssam <borgssam@gmail.com>
2024-07-16 15:38:10 +0900

    4장 리액트기본 => 3.컴포넌트 사용방법

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+function Ul(){
+  return (
+    <ul>
+      <li>강아지</li>
+      <li>토끼</li>
+      <li>고양이</li>
+      <li>거북이</li>
+    </ul>
+  );
+}
+function Table(){
+  return (
+    <table border='1px'>
+      <tr>
+        <td>이름</td>
+        <td>나이</td>
+      </tr>
+      <tr>
+        <td>홍길동2</td>
+        <td>24</td>
+      </tr>
+      <tr>
+        <td>이순신2</td>
+        <td>60</td>
+      </tr>
+    </table>
 
+  );
+}
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>강아지</li>
-        <li>토끼</li>
-        <li>고양이</li>
-        <li>거북이</li>
-      </ul>
+      <Ul></Ul>
+      <Table></Table>
       
-      <table border='1px'>
-        <tr>
-          <td>이름</td>
-          <td>나이</td>
-        </tr>
-        <tr>
-          <td>홍길동</td>
-          <td>24</td>
-        </tr>
-        <tr>
-          <td>이순신</td>
-          <td>60</td>
-        </tr>
-      </table>
     </div>
   );
 }
