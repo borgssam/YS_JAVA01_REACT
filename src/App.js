6f9d0971da6536fffc2d457a8c5669306b308167
borgssam <borgssam@gmail.com>
2024-07-16 15:38:48 +0900

    4장 리액트기본 => 3.컴포넌트사용방법02

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-function Ul(){
-  return (
-    <ul>
-      <li>강아지</li>
-      <li>토끼</li>
-      <li>고양이</li>
-      <li>거북이</li>
-    </ul>
-  );
-}
-function Table(){
-  return (
-    <table border='1px'>
-      <tr>
-        <td>이름</td>
-        <td>나이</td>
-      </tr>
-      <tr>
-        <td>홍길동2</td>
-        <td>24</td>
-      </tr>
-      <tr>
-        <td>이순신2</td>
-        <td>60</td>
-      </tr>
-    </table>
 
-  );
-}
-function App() {
+export function App2(){
+  const divStyle = {
+    border: '1px solid blue'
+  }
   return (
-    <div className="App">
-      <Ul></Ul>
-      <Table></Table>
-      
+    <div style={divStyle}>
+    <p>안녕하세요</p>
+    <p>반갑습니다.</p>
     </div>
   );
 }
-
-export default App;
