1e05807fea977b8f09e699c140747f119cb61b09
borgssam <borgssam@gmail.com>
2024-07-17 12:32:43 +0900

    4장 리액트기본: 회원목록

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import {App} from './App002';
+import {App} from './App003';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
