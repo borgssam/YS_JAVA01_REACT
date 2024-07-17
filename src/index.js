1248e235035d30e2ea3edf458a3273acbb5bbd45
borgssam <borgssam@gmail.com>
2024-07-17 10:39:20 +0900

    4장 리액트기본 로그인박스

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import {App} from './App001';
+import {App} from './App002';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
