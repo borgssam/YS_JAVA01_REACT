486432a12eb39b7aa8c49aa48bf23b49a83fe28c
borgssam <borgssam@gmail.com>
2024-07-18 12:27:40 +0900

    step11 훅 useState

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //파일정보:src/index.js
 import ReactDOM from 'react-dom/client';
-import {App} from './step10/App'; //<---
+import {App} from './step11/App'; //<---
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
