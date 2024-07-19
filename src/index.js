2bdcb5a23b78027d2a1a60fd3145e577d58a2356
borgssam <borgssam@gmail.com>
2024-07-19 11:47:30 +0900

    step13 (이벤트처리)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //파일정보:src/index.js
 import ReactDOM from 'react-dom/client';
 //import {App} from './step_test/App';
-import {App} from './step12/App'; //<---
+import {App} from './step13/App'; //<---
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
